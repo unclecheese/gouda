c56458317f177ad43a7eae9aceab43aff5754de9
Uncle Cheese <unclecheese@leftandmain.com>
2013-12-05 15:35:58 +1300

    begin block commenting work

--- src/core/core.js
+++ src/core/core.js
@@ -1,7 +1,7 @@
 define([], function() {
 
   "use strict";
-  
+
   var Core = {};
 
   Core.Config = {};
@@ -71,6 +71,142 @@ define([], function() {
 
 
   };
+
+
+  Core.Parser = {
+
+    RXP_OUTPUT_VAR: "\\{=\\s*(.*)\\s*\\}",
+
+    RXP_IF_BLOCK: "\\{\\s*if\\s(.*)\\s*\\}",
+
+    RXP_ELSE_BLOCK: "\\{\\s*else\\s*\\}",
+
+    RXP_END_IF: "\\{\\s*endif\\s*\\}",
+
+    RXP_LOOP_BLOCK: "\\{\\s*loop\\s(.*)\\s*\\}",
+
+    RXP_END_LOOP: "\\{\\s*endloop\\s*\\}",
+
+    ifStack: [],
+
+    loopStack: [],
+
+    ifOpenDepths: [],
+
+    loopOpenDepths: [],
+
+    ifNegation: false,
+
+    parseComments: function (node, depth) {
+      var nodes = node.childNodes,
+          nLen = nodes.length,
+          i = 0,
+          current, data, block;
+
+      if(!depth) depth = 1;
+
+      for(i in nodes) {
+        current = nodes[i];
+        if (current.nodeType === 1) {
+            if((block = this._currentIfBlock()) && (this._currentIfDepth() <= depth)) {
+              console.log(block);
+              current.setAttribute(this.ifNegation ? "cydr-hidden" : "cydr-visible", block);
+            }
+            else if((block = this._currentLoopBlock()) && (this._currentLoopDepth() <= depth)) {
+              current.setAttribute("cydr-repeat", block);
+            }
+            else {
+              this.parseComments(current, depth+1);
+              continue;
+            }
+        }
+
+        data = (current.nodeType === 8) ? current.data : (current.nodeType === 3 ? current.nodeValue : false);
+
+        if(!data || this._matchTag(current, data, depth)) {
+          continue;
+        }
+
+      }
+    },
+
+    _matchTag: function (current, data, depth) {
+      return this._matchOutput(current, data) ||
+             this._matchLoopBlock(current, data, depth) ||
+             this._matchIfBlock(current, data, depth);
+    },
+
+    _matchOutput: function (current, data) {
+      var replacement,
+          matches = data.match(new RegExp(this.RXP_OUTPUT_VAR));
+      if(matches) {
+        replacement = document.createElement("SPAN");
+        replacement.setAttribute("cydr-content", Core.Utils.trim(matches[1]));
+        current.parentNode.replaceChild(replacement ,current);
+        return true;
+      }
+
+      return false;
+    },
+
+    _matchLoopBlock: function (current, data, depth) {
+      var matches, nodes;
+          nodes;
+      if(matches = data.match(new RegExp(this.RXP_LOOP_BLOCK))) {
+        this.loopStack.push(Core.Utils.trim(matches[1]));
+        this.loopOpenDepths.push(depth);
+        return true;
+      }
+      else if(matches = data.match(new RegExp(this.RXP_END_LOOP))) {
+        this.loopStack.pop();
+        this.loopOpenDepths.pop();
+        return true;
+      }
+
+      return false;
+    },
+
+    _matchIfBlock: function (current, data, depth) {
+      var matches, nodes;
+          nodes;
+      if(matches = data.match(new RegExp(this.RXP_IF_BLOCK))) {
+        console.log("found if block!!", matches);
+        this.ifNegation = false;
+        this.ifStack.push(Core.Utils.trim(matches[1]));
+        this.ifOpenDepths.push(depth);
+        return true;
+      }
+      else if(matches = data.match(new RegExp(this.RXP_ELSE_BLOCK))) {
+        this.ifNegation = true;
+        return true;
+      }
+      else if(matches = data.match(new RegExp(this.RXP_END_IF))) {
+        this.ifStack.pop();
+        this.ifOpenDepths.pop();
+        return true;
+      }
+
+      return false;
+    },
+
+    _currentIfBlock: function () {
+      return this.ifStack.length ? this.ifStack[this.ifStack.length-1] : false;
+    },
+
+    _currentLoopBlock: function () {
+      return this.loopStack.length ? this.loopStack[this.loopStack.length-1] : false;
+    },
+
+    _currentIfDepth: function () {
+      return this.ifOpenDepths.length ? this.ifOpenDepths[this.ifOpenDepths.length-1] : false;
+    },
+
+    _currentLoopDepth: function () {
+      return this.loopOpenDepths.length ? this.loopOpenDepths[this.loopOpenDepths.length-1] : false;
+    }
+
+
+  }
 
   return Core;
 });
--- src/model/viewmodel.js
+++ src/model/viewmodel.js
@@ -6,7 +6,7 @@ define(['./model','../core/core'], function(Model, Core) {
 
 	  _className: "ViewModel",
 
-	  _selector: "body",	  
+	  _selector: "body",
 
 	  __construct: function (selector) {
 	    this._selector = selector;
@@ -14,36 +14,18 @@ define(['./model','../core/core'], function(Model, Core) {
 	  },
 
 
-	  run: function (onReadyCallback) {	  	
+	  run: function (onReadyCallback) {
 	    var node = document.querySelector(this._selector);
-	    this._parseComments(node);
+	    Core.Parser.parseComments(node);
 	    if (node) {
 	      this.applyBindingsToNode(node);
 	      if(onReadyCallback) {
 	      	onReadyCallback.call(this);
 	      }
 	    }
-	  },
-
-
-	  _parseComments: function (node) {
-	  	var nodes = node.childNodes;
-	  	var nLen = nodes.length;
-	  	var current, matches, replacement;
-        while (nLen--) {
-            current = nodes[nLen];
-            if (current.nodeType !== 8) {
-                this._parseComments(current);
-                continue;
-            }
-            matches = current.data.match(/\{%=\s*(.*)\s*%\}/);
-			if(matches) {
-				replacement = document.createElement("SPAN");
-				replacement.setAttribute("cydr-content", Core.Utils.trim(matches[1]));
-				current.parentNode.replaceChild(replacement ,current);
-			}
-        }
 	  }
+
+
 	});
 
 	return ViewModel;
